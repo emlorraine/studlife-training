e24550cb09a542c2057f5c74529e7d99119edf2f
Emma Baker <emma.baker@wustl.edu>
2020-08-12 20:01:03 -0500

    We broke things but then we fixed them

--- studlife-training/src/components/Main.js
+++ studlife-training/src/components/Main.js
@@ -1,75 +1,38 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
-
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs'
+import 'react-tabs/style/react-tabs.css';
 import './Main.css';
-import jsonData from '../data/data.json';
 
-const modules = ['Welcome to Student Life', 'Reporting 101', 'Washington University 101', 'News & Writing from Reporting', 'Opinion & Writing from Experience' 'Inclusive Reporting'];
-const studLifeData = Object.values(jsonData);
-const reportingData = Object.values(jsonData);
-const washUData = Object.values(jsonData);
-const newsData = Object.values(jsonData);
-const opinionData = Object.values(jsonData);
-const inclusiveReportingData = Object.values(jsonData);
+const modules = ['Welcome to Student Life', 'Reporting 101', 'Washington University 101', 'News & Writing from Reporting', 'Opinion & Writing from Experience', 'Inclusive Reporting'];
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        selectWelcome: null,
-        studLifeData: null,
-        reportingData: null,
-        washUData: null,
-        newsData: null,
-        opinionData: null,
-        inclusiveReportingData: null,
     };
-    this.selectWelcome = this.selectWelcome.bind(this);
-    this.selectReporting = this.selectReporting.bind(this);
-    this.selectWashU = this.selectWashU.bind(this);
-    this.selectNews = this.selectNews.bind(this);
-    this.selectOpinion = this.selectWashU.bind(this);
-    this.selectInclusive = this.selectNews.bind(this);
-  }
-
-  selectWelcome(){
-
-  }
-
-  selectReporting(){
-    this.setState({}); 
-  }
-
-  selectWashU(){
-    this.setState({}); 
-  }
-
-  selectNews(){
-    this.setState({}); 
-  }
-
-  selectOpinion(){
-    this.setState({}); 
-  }
-
-  selectInclusive(){
-    this.setState({}); 
   }
 
   render() {
     return (
-      <Container fluid={true} className="housing-container px-5">
-        <Row>
-          {/* <Header /> */}
-          <Col md="6" className="listings">
-            <Row>
-            </Row>
-          </Col>
-          <Col md="6" className="side-map pl-0" id="side-map-container">
-          </Col>
-        </Row>
-      </Container>
-    );
+        <Tabs defaultIndex = {1} onSelect={index => console.log(index)}>
+          <TabList>
+            <Tab>Welcome to Student Life</Tab>
+            <Tab>Reporting 101</Tab>
+            <Tab>The basics of Washington University</Tab>
+            <Tab>News and Writing from Reporting</Tab>
+            <Tab>Opinion and Writing from Experience</Tab>
+            <Tab>Inclusive Reporting</Tab>
+          </TabList>
+
+          <TabPanel>Text for tab 1</TabPanel>
+          <TabPanel>Text for tab 2</TabPanel>
+          <TabPanel>Text for tab 3</TabPanel>
+          <TabPanel>Text for tab 4</TabPanel>
+          <TabPanel>Text for tab 5</TabPanel>
+          <TabPanel>Text for tab 6</TabPanel>
+        </Tabs>
+    )
   }
 }
 
