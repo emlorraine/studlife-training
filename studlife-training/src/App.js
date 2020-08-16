dc08da974e947ded6df1aa913b730c8f5155f3cd
Emma Baker <emma.baker@wustl.edu>
2020-08-16 00:34:30 -0500

    Making link routes

--- studlife-training/src/App.js
+++ studlife-training/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import Main from './components/Main.js';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navigation from './Navigation';
+
 import './App.css';
 
 function App() {
   return (
     <div className="root">
-      <Main />
+      {/* <Main /> */}
+    <Router>
+      <Navigation />
+    </Router>
     </div>
   );
 }
