6ae0235a918a666d9592a7a2bd5788d293f5562e
Emma Baker <emma.baker@wustl.edu>
2020-08-16 16:00:07 -0500

    ~data~

--- studlife-training/src/components/Main.js
+++ studlife-training/src/components/Main.js
@@ -7,26 +7,12 @@ import './Main.css';
 import { GoogleSpreadsheet } from "google-spreadsheet";
 
 const { GoogleSpreadsheet } = require('google-spreadsheet');
-const private_sheet_key =  "\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDfQfHa3BAu3Vqu\nPebFgCc7TixQQy1XFSuYgWmGoxW01ITcWAoFF611M9UmOBw5n5NcdHbYC63VMKPB\nwOfNkaohgl2LYEuLc35upJA579OaR4Q2Fuj7up2HoUOU0j6VVSPs5pZMMVUWdYxw\nK+cQvOexXywvzHzOACZaqzndvR3DDtXbvuRNs8Af8rpRm+lqiSFFsSJmeGb1bKBT\nRVWrzHG2H1rSKaQqypUoChuSLO/Ye9A1g5t0ZWES8MLY6OyA+9DqVqlQixxc/7qg\naGxHBHvPFzs5F5DLzRpjtbR82mhCTgnZf+RShEMmJP6ub2IgHGIzAzGwZ7Ai6Zb2\nPOKKeI/JAgMBAAECggEAFoIq2hU5ucqDVpx9ubJpQidoJvy+2NAU7HPk/w1OJkRk\nKLrk4mKNZbWWFcZZF3jT0zcYDEFmgzSQ2KXpjjt+BZGD0Nt316q14n3LigEjOk4K\nMacu/MIcbmIp0sU8811Ow+xI1uGvtxX0KhL4YXhKJlh2t1L3JOW1ot/ZyQCaEYzV\nwvNVgxAAq+n8GBJDnslhtOTSd5JHa+gqrpIW1XDJxKQ62YmTGEdBmGOm+NsHoNyV\nl5CcZLpsQ6Y/Ao1uI/wS6O2vPj+P0hwN3KAmzYFpCg3jR4VjY+N0C21qZubuhSKu\n72AcCpmiP2P0uGppr9owmA74fYeUKFuZl4WfsQZPaQKBgQD6FZnV1gyRLE3+xjS6\nhatPsXKH7VUPgRApFdgyA0D7Wyn4tKYX48vz1ZT9IDaZgafwz+j7e+tUYRq+MI/x\n2nTQTt1YEDb0hiREAuTjr4W5757PBSWbCFdoezTf25dFCDXRGxABpi87uYBH//qg\noZ5Jpni70/a7NzHQvwT/CbguWwKBgQDkieSRM/Im6/+M14kitpeYC5V+e/NPrCtx\nlJkf9QQRH87j/zmL8n3SSJFmzQM1JueqpX/QqOOajLLdLzbUuqP5hFVHLiOeXAqA\nK/2t7eNKvYuxFpo016BD4/PHun9/pMbuqVhgg8QXKrEbjwDGkoP3wuDDOmHBSg7Z\nSU3FcAobqwKBgQCcAqW5VJL6bZ0qdxOl2IWKbbR5P4F07praRppceNMs+y4vzOJg\nvOca8f7D+jiVguD1xX4ehKvGM92vEDR8OD/vQ4Op0FO4L4aoNvX7ZCAUYnu7wgY3\ntPhyYYoWrLKtifWowS03GikVJR/1yqbbVneckpF2+0BMgp49Dy1avQF9SQKBgEIq\nYhBf97WpEd2W8U8pig4tHGT4QeNJN3K00PhdmYXTrbR4yhJobGEbPW2D4W/cCapw\nK+cfXnX6UjaTWs6QErVEyOPIyespPqkJE3LtsqaK5gypPJAb9XxyXLvwlMVB0Y57\nXOjkfTsv0wLljhXviWGL/ELJfk/VvyLNhESrQWHtAoGBANSglqDFZqqZM6o6kF+2\nYQ1LWZ9ZiN5TEG5efumfHaA5Y0yCfBeBBY2p9nsQdLjEM2nYq8QY+bDQEd9mOu3d\nWvnMr9c+72k4JwZmnfHCioN1c0/cRf7exK641dD/KqFRp91cy/KkMOUenxKXTU1y\ngQjZ8SOAKxdIb5qsRbdXVgRa\n-----END PRIVATE KEY-----\n",
+const private_sheet_key = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCb51g+UpK3ILUu\nzeMUzYRKpNW9Yv3QDICMAfRZLxgAzinJiLyaFMYXHjSlQ1eMmnygA+bTDowTDrPM\nrru0Akc4it0YqQRoaEMsM6EYgOMmKfFfk17c7CQyEZnWCjK9AqhKrMbK1UPosPpT\nqxBHwBnXhcu62wzYRUghB+rTbU77ygaw7sFSwd/kifSTAF9CCMRicxtFJoTEjHnV\n4qkpJFZPKFS9/w/hBdxsK/JzQx8yt11mmU9+SoeD/DNEvxwlIeBtbj5Bh5IdA626\noMXVNA4y7+Jk6WoUPZuj6fclKGQLi7fn6iCvf5Mnt7AdcFSZzvNEqUOwygZK+KHK\njQm880bZAgMBAAECggEADklJU1CyxBg5siPIws/AZyyqr3OiSY3Tu8C0GPYRT4WU\n27Rtf/sWWej5Lwm4PJitgeZJoR6MpHRs8n0ykyDAUQSclRVr43tmaGcExJ+O8SDX\nzwfMohb7jCEDxVLi6mITdOBXSR/43JpXHhKnZ8PJOnA2xpUrHKhgnzJACQbTvYvV\noTekP0tnBssKbaLGQUzWLitUiWcxcsOW3Ywx82cREvyJ8zVJPB0Qdbo4MhGFZIM+\ndKfWnO/cs/HvQxZc8TpILfwX+Sna21lp1lSLDB5N/sIUVrw3od20pMqz1MDfBXvk\nvaV+YvgoUgNB+i4tcqJWeyUV7S7ThDTROkgXbn7k+QKBgQDRFsvf59qU2qnomanc\nMusY1sk7C0rNs2SgaDEJRJOOAur1LiszbOOe53/fd4w5b/H5Pu28RaWPduchZ4/Q\nRO6bT2Mob9j8C4tqsY8LDj4JOz6W+UIMP1iveYHMEVOgT3nwMPD7myk1nUWVceU1\ndroa4IyzWT0cqzrCSlpJPK+nbQKBgQC+4c2QG7HuVieGGDEReYKtUHmHBJBgjl9j\nN2I7EwzZ261AiitbUVDJWHskFgGlinm6hdJwo3hf4PprMG5c1nNHfj37/0IZHjmn\nwQiiQmq1JQuN7W5blxHfdnYJzvqbLkBG+ksjOgoiiXZCw5QsrehF8dhxIIzRn46U\nnwaUp6NdnQKBgAYPzyFSq3edT0e7DL2hjEdQEs1val/ET6qG2sUWA+EeHIm3KPF4\nurCkrYRhBuOzZyR+uo22xzH9lH3Xbsfu96vxBULq2zOTchtJwofjhdDmss3VtCB+\nxadwsl6kMfR32WUpRhNFUFCJmrbWcnq2UnbtA6c/cxIdtLplRXDSHZWRAoGBAIzK\nmken1dbY3zAuGcss1/EkAW9QM3m0qbDRwfeWtozeVmzbPN2PHcpVJAba4H4MpHqq\niCyitn/pvmtffpelacQa3ndHsKY8QAR3AfpEiNZxq5MUxKhzxjAP/UKA0yGwvKJd\nrRwfjCgrQfo+rcVCxKcKmj3e518alB0VKtJsz7eVAoGASiCHUhHtYUOkquGNL7PD\nJHJWd6fUUhACrmeENN45j6wlLU/JQsfSkNOLwGDk/oNR1tc/UJxCWeVUSSkIoz/Z\n313YENPtZoxmHEV7GVFF4kKgX0JeJAjC3cRqW73J8iUh9jI1ycMf8GD2XuF7bC19\nO7+kByGGdKJ6hWylM4oxC3I=\";
+const client_email =  "studlife-training-sheets@studlife-training-sheets.iam.gserviceaccount.com";
 
 const modules = ['Welcome to Student Life', 'Reporting 101', 'Washington University 101', 'News & Writing from Reporting', 'Opinion & Writing from Experience', 'Inclusive Reporting'];
 const doc = new GoogleSpreadsheet('1vFolZ0BmiI2VclYyigkHfgRs1lAMdLMUvyMgKFMDuIU');
 
-const appendSpreadsheet = async (row) => {
-  try {
-    await doc.useServiceAccountAuth({
-      client_email: 'studlife-training-sheets@studlife-training-286617.iam.gserviceaccount.com',
-      private_key: private_sheet_key,
-    });
-    await doc.loadInfo();
-    const sheet = doc.sheetsById[0];
-    const result = await sheet.addRow(row);
-  } catch (e) {
-    console.error('Error: ', e);
-  }
-};
-const newRow = {userName: "",	welcomeInput: "",	reportingInput: "",	washuInput: "",	newsInput: "",	opinionInput: "",	inclusiveInput: ""}; 
-appendSpreadsheet(newRow);
 
 class Main extends React.Component {
   constructor(props) {
@@ -61,7 +47,22 @@ class Main extends React.Component {
       opinionInput: this.state.opinionInput,
       inclusiveInput: this.state.inclusiveInput
     }
-    itemsRef.push(item);
+    const appendSpreadsheet = async (row) => {
+      try {
+        await doc.useServiceAccountAuth({
+          client_email: 'studlife-training-sheets@studlife-training-286617.iam.gserviceaccount.com',
+          private_key: private_sheet_key,
+        });
+        await doc.loadInfo();
+        const sheet = doc.sheetsById[0];
+        const result = await sheet.addRow(row);
+      } catch (e) {
+        console.error('Error: ', e);
+      }
+    };
+    const newRow = {userName: "",	welcomeInput: "",	reportingInput: "",	washuInput: "",	newsInput: "",	opinionInput: "",	inclusiveInput: ""}; 
+    appendSpreadsheet(newRow);
+
     this.setState({
       welcomeInput: '',
       reportingInput: '',
