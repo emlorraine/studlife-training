836754726e51a231b8f9bbb4955d18472bdea98f
Emma Baker <emma.baker@wustl.edu>
2020-08-16 20:16:17 -0500

    Google API madness

--- studlife-training/src/components/Main.js
+++ studlife-training/src/components/Main.js
@@ -6,13 +6,12 @@ import firebase from './firebase.js';
 import './Main.css';
 import { GoogleSpreadsheet } from "google-spreadsheet";
 
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const private_sheet_key = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCb51g+UpK3ILUu\nzeMUzYRKpNW9Yv3QDICMAfRZLxgAzinJiLyaFMYXHjSlQ1eMmnygA+bTDowTDrPM\nrru0Akc4it0YqQRoaEMsM6EYgOMmKfFfk17c7CQyEZnWCjK9AqhKrMbK1UPosPpT\nqxBHwBnXhcu62wzYRUghB+rTbU77ygaw7sFSwd/kifSTAF9CCMRicxtFJoTEjHnV\n4qkpJFZPKFS9/w/hBdxsK/JzQx8yt11mmU9+SoeD/DNEvxwlIeBtbj5Bh5IdA626\noMXVNA4y7+Jk6WoUPZuj6fclKGQLi7fn6iCvf5Mnt7AdcFSZzvNEqUOwygZK+KHK\njQm880bZAgMBAAECggEADklJU1CyxBg5siPIws/AZyyqr3OiSY3Tu8C0GPYRT4WU\n27Rtf/sWWej5Lwm4PJitgeZJoR6MpHRs8n0ykyDAUQSclRVr43tmaGcExJ+O8SDX\nzwfMohb7jCEDxVLi6mITdOBXSR/43JpXHhKnZ8PJOnA2xpUrHKhgnzJACQbTvYvV\noTekP0tnBssKbaLGQUzWLitUiWcxcsOW3Ywx82cREvyJ8zVJPB0Qdbo4MhGFZIM+\ndKfWnO/cs/HvQxZc8TpILfwX+Sna21lp1lSLDB5N/sIUVrw3od20pMqz1MDfBXvk\nvaV+YvgoUgNB+i4tcqJWeyUV7S7ThDTROkgXbn7k+QKBgQDRFsvf59qU2qnomanc\nMusY1sk7C0rNs2SgaDEJRJOOAur1LiszbOOe53/fd4w5b/H5Pu28RaWPduchZ4/Q\nRO6bT2Mob9j8C4tqsY8LDj4JOz6W+UIMP1iveYHMEVOgT3nwMPD7myk1nUWVceU1\ndroa4IyzWT0cqzrCSlpJPK+nbQKBgQC+4c2QG7HuVieGGDEReYKtUHmHBJBgjl9j\nN2I7EwzZ261AiitbUVDJWHskFgGlinm6hdJwo3hf4PprMG5c1nNHfj37/0IZHjmn\nwQiiQmq1JQuN7W5blxHfdnYJzvqbLkBG+ksjOgoiiXZCw5QsrehF8dhxIIzRn46U\nnwaUp6NdnQKBgAYPzyFSq3edT0e7DL2hjEdQEs1val/ET6qG2sUWA+EeHIm3KPF4\nurCkrYRhBuOzZyR+uo22xzH9lH3Xbsfu96vxBULq2zOTchtJwofjhdDmss3VtCB+\nxadwsl6kMfR32WUpRhNFUFCJmrbWcnq2UnbtA6c/cxIdtLplRXDSHZWRAoGBAIzK\nmken1dbY3zAuGcss1/EkAW9QM3m0qbDRwfeWtozeVmzbPN2PHcpVJAba4H4MpHqq\niCyitn/pvmtffpelacQa3ndHsKY8QAR3AfpEiNZxq5MUxKhzxjAP/UKA0yGwvKJd\nrRwfjCgrQfo+rcVCxKcKmj3e518alB0VKtJsz7eVAoGASiCHUhHtYUOkquGNL7PD\nJHJWd6fUUhACrmeENN45j6wlLU/JQsfSkNOLwGDk/oNR1tc/UJxCWeVUSSkIoz/Z\n313YENPtZoxmHEV7GVFF4kKgX0JeJAjC3cRqW73J8iUh9jI1ycMf8GD2XuF7bC19\nO7+kByGGdKJ6hWylM4oxC3I=\";
+// const { GoogleSpreadsheet } = require('google-spreadsheet');
+// var private_sheet_key = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCb51g+UpK3ILUu\nzeMUzYRKpNW9Yv3QDICMAfRZLxgAzinJiLyaFMYXHjSlQ1eMmnygA+bTDowTDrPM\nrru0Akc4it0YqQRoaEMsM6EYgOMmKfFfk17c7CQyEZnWCjK9AqhKrMbK1UPosPpT\nqxBHwBnXhcu62wzYRUghB+rTbU77ygaw7sFSwd/kifSTAF9CCMRicxtFJoTEjHnV\n4qkpJFZPKFS9/w/hBdxsK/JzQx8yt11mmU9+SoeD/DNEvxwlIeBtbj5Bh5IdA626\noMXVNA4y7+Jk6WoUPZuj6fclKGQLi7fn6iCvf5Mnt7AdcFSZzvNEqUOwygZK+KHK\njQm880bZAgMBAAECggEADklJU1CyxBg5siPIws/AZyyqr3OiSY3Tu8C0GPYRT4WU\n27Rtf/sWWej5Lwm4PJitgeZJoR6MpHRs8n0ykyDAUQSclRVr43tmaGcExJ+O8SDX\nzwfMohb7jCEDxVLi6mITdOBXSR/43JpXHhKnZ8PJOnA2xpUrHKhgnzJACQbTvYvV\noTekP0tnBssKbaLGQUzWLitUiWcxcsOW3Ywx82cREvyJ8zVJPB0Qdbo4MhGFZIM+\ndKfWnO/cs/HvQxZc8TpILfwX+Sna21lp1lSLDB5N/sIUVrw3od20pMqz1MDfBXvk\nvaV+YvgoUgNB+i4tcqJWeyUV7S7ThDTROkgXbn7k+QKBgQDRFsvf59qU2qnomanc\nMusY1sk7C0rNs2SgaDEJRJOOAur1LiszbOOe53/fd4w5b/H5Pu28RaWPduchZ4/Q\nRO6bT2Mob9j8C4tqsY8LDj4JOz6W+UIMP1iveYHMEVOgT3nwMPD7myk1nUWVceU1\ndroa4IyzWT0cqzrCSlpJPK+nbQKBgQC+4c2QG7HuVieGGDEReYKtUHmHBJBgjl9j\nN2I7EwzZ261AiitbUVDJWHskFgGlinm6hdJwo3hf4PprMG5c1nNHfj37/0IZHjmn\nwQiiQmq1JQuN7W5blxHfdnYJzvqbLkBG+ksjOgoiiXZCw5QsrehF8dhxIIzRn46U\nnwaUp6NdnQKBgAYPzyFSq3edT0e7DL2hjEdQEs1val/ET6qG2sUWA+EeHIm3KPF4\nurCkrYRhBuOzZyR+uo22xzH9lH3Xbsfu96vxBULq2zOTchtJwofjhdDmss3VtCB+\nxadwsl6kMfR32WUpRhNFUFCJmrbWcnq2UnbtA6c/cxIdtLplRXDSHZWRAoGBAIzK\nmken1dbY3zAuGcss1/EkAW9QM3m0qbDRwfeWtozeVmzbPN2PHcpVJAba4H4MpHqq\niCyitn/pvmtffpelacQa3ndHsKY8QAR3AfpEiNZxq5MUxKhzxjAP/UKA0yGwvKJd\nrRwfjCgrQfo+rcVCxKcKmj3e518alB0VKtJsz7eVAoGASiCHUhHtYUOkquGNL7PD\nJHJWd6fUUhACrmeENN45j6wlLU/JQsfSkNOLwGDk/oNR1tc/UJxCWeVUSSkIoz/Z\n313YENPtZoxmHEV7GVFF4kKgX0JeJAjC3cRqW73J8iUh9jI1ycMf8GD2XuF7bC19\nO7+kByGGdKJ6hWylM4oxC3I=\";
 const client_email =  "studlife-training-sheets@studlife-training-sheets.iam.gserviceaccount.com";
-
-const modules = ['Welcome to Student Life', 'Reporting 101', 'Washington University 101', 'News & Writing from Reporting', 'Opinion & Writing from Experience', 'Inclusive Reporting'];
 const doc = new GoogleSpreadsheet('1vFolZ0BmiI2VclYyigkHfgRs1lAMdLMUvyMgKFMDuIU');
 
+const modules = ['Welcome to Student Life', 'Reporting 101', 'Washington University 101', 'News & Writing from Reporting', 'Opinion & Writing from Experience', 'Inclusive Reporting'];
 
 class Main extends React.Component {
   constructor(props) {
@@ -25,43 +24,71 @@ class Main extends React.Component {
       opinionInput: '',
       inclusiveInput: ''
     };
-    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this); 
+    this.handleWelcomeChange = this.handleWelcomeChange.bind(this);
+    this.handleReportingChange = this.handleReportingChange.bind(this);
+    this.handleWashUChange = this.handleWashUChange.bind(this);
+    this.handleNewsChange = this.handleNewsChange.bind(this);
+    this.handleOpinionChange = this.handleOpinionChange.bind(this);
+    this.handleInclusiveChange = this.handleInclusiveChange.bind(this);
   }
-  handleChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+  handleWelcomeChange(e) {
+    this.setState({welcomeInput: e.target.value});
+    console.log(e.target.value); 
   }
 
+  handleReportingChange(e) {
+    this.setState({reportingInput: e.target.value});
+    console.log(e.target.value); 
+  }
 
+  handleWashUChange(e) {
+    this.setState({washuInput: e.target.value});
+    console.log(e.target.value); 
+  }  
+  
+  handleNewsChange(e) {
+    this.setState({newsInput: e.target.value});
+    console.log(e.target.value); 
+  }
+
+  handleOpinionChange(e) {
+    this.setState({opinionInput: e.target.value});
+    console.log(e.target.value); 
+  }
+
+  handleInclusiveChange(e) {
+    this.setState({inclusiveInput: e.target.value});
+    console.log(e.target.value); 
+  }
 
   handleSubmit(e) {
+    console.log(e); 
     e.preventDefault();
-    const itemsRef = firebase.database().ref('items');
-    const item = {
-      welcomeInput: this.state.welcomeInput,
-      reportingInput: this.state.reportingInput,
-      washuInput: this.state.washuInput,
-      newsInput: this.state.newsInput,
-      opinionInput: this.state.opinionInput,
-      inclusiveInput: this.state.inclusiveInput
-    }
-    const appendSpreadsheet = async (row) => {
-      try {
-        await doc.useServiceAccountAuth({
-          client_email: 'studlife-training-sheets@studlife-training-286617.iam.gserviceaccount.com',
-          private_key: private_sheet_key,
-        });
-        await doc.loadInfo();
-        const sheet = doc.sheetsById[0];
-        const result = await sheet.addRow(row);
-      } catch (e) {
-        console.error('Error: ', e);
-      }
-    };
-    const newRow = {userName: "",	welcomeInput: "",	reportingInput: "",	washuInput: "",	newsInput: "",	opinionInput: "",	inclusiveInput: ""}; 
-    appendSpreadsheet(newRow);
+    // const itemsRef = firebase.database().ref('items');
+    // const item = {
+     var sendWelcomeInput = this.state.welcomeInput;
+     var sendReportingInput = this.state.reportingInput;
+     var sendWashuInput = this.state.washuInput;
+     var sendNewsInput = this.state.newsInput;
+     var sendOpinionInput = this.state.opinionInput;
+     var sendInclusiveInput = this.state.inclusiveInput;
+    // }
+    // const appendSpreadsheet = async (row) => {
+    //   try {
+    //     await doc.useServiceAccountAuth({
+    //       client_email: 'studlife-training-sheets@studlife-training-286617.iam.gserviceaccount.com',
+    //       // private_key: private_sheet_key,
+    //     });
+    //     await doc.loadInfo();
+    //     const sheet = doc.sheetsById[0];
+    //     const result = await sheet.addRow(row);
+    //   } catch (e) {
+    //     console.error('Error: ', e);
+    //   }
+    // };
+    // const newRow = {userName: "",	welcomeInput: "",	reportingInput: "",	washuInput: "",	newsInput: "",	opinionInput: "",	inclusiveInput: ""}; 
+    // appendSpreadsheet(newRow);
 
     // this.setState({
     //   welcomeInput: '',
@@ -112,7 +139,7 @@ render() {
           Text for tab 1
             <form>
               <form onSubmit={this.handleSubmit}>
-              <input type="text" name="welcomeInput" placeholder="Filler text" onChange={this.handleChange} value={this.state.welcomeInput}/>
+              <input type="text" name="welcomeInput" placeholder="Filler text" onChange={this.handleWelcomeChange} value={this.state.welcomeInput}/>
                 <button>Submit</button>
               </form>
             </form>
@@ -120,35 +147,35 @@ render() {
         <TabPanel>
           Text for tab 2
             <form>
-              <input type="text" name="reportingInput" placeholder="Filler text" onChange={this.handleChange} value={this.state.reportingInput}/>
+              <input type="text" name="reportingInput" placeholder="Filler text" onChange={this.handleReportingChange} value={this.state.reportingInput}/>
               <button>Submit</button>
             </form>
         </TabPanel>
         <TabPanel>
           Text for tab 3
             <form>
-              <input type="text" name="washuInput" placeholder="Filler text" onChange={this.handleChange} value={this.state.washuInput}/>
+              <input type="text" name="washuInput" placeholder="Filler text" onChange={this.handleWashUChange} value={this.state.washuInput}/>
               <button>Submit</button>
             </form>
         </TabPanel>
         <TabPanel>
           Text for tab 4
             <form>
-              <input type="text" name="newsInput" placeholder="Filler text" onChange={this.handleChange} value={this.state.newsInput}/>
+              <input type="text" name="newsInput" placeholder="Filler text" onChange={this.handleNewsChange} value={this.state.newsInput}/>
               <button>Submit</button>
             </form>
             </TabPanel>
         <TabPanel>
           Text for tab 5
             <form>
-              <input type="text" name="opinionInput" placeholder="Filler text" onChange={this.handleChange} value={this.state.opinionInput}/>
+              <input type="text" name="opinionInput" placeholder="Filler text" onChange={this.handleOpinionChange} value={this.state.opinionInput}/>
               <button>Submit</button>
             </form>
             </TabPanel>
         <TabPanel>
           Text for tab 6
             <form>
-              <input type="text" name="inclusiveInput" placeholder="Filler text" onChange={this.handleChange} value={this.state.inclusiveInput}/>
+              <input type="text" name="inclusiveInput" placeholder="Filler text" onChange={this.handleInclusiveChange} value={this.state.inclusiveInput}/>
               <button>Submit</button>
             </form>
             </TabPanel>
