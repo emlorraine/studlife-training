9b3da34b17fa70d079c318f145359d71173d8fdb
Emma Baker <emma.baker@wustl.edu>
2020-08-28 21:05:52 -0500

    Hate CSS

--- studlife-training/src/components/Nav.js
+++ studlife-training/src/components/Nav.js
@@ -17,16 +17,12 @@ class Nav extends Component {
   refWritingReporting = React.createRef()
   refWritingExperience = React.createRef()
 
-  // constructor(props) {
-  //   super(props);
-  //   this.scrollToContent = this.scrollToContent.bind(this);
-  // }
   componentDidMount() {
-    this.setState({ loaded: true })
+    this.setState(
+      { loaded: true })
   }
 
   handleScrollTo = (elRef) => {
-    // Incase the ref supplied isn't ref.current
     const el = elRef.current ? elRef.current : elRef
     el.scrollIntoView({
       behavior: 'smooth',
@@ -41,46 +37,46 @@ class Nav extends Component {
         </header>
         <nav>
           <ul>
-            <li onClick={() => { this.handleScrollTo(this.refWelcome) }}>
+            <li onClick={() => { this.handleScrollTo(this.refWelcome) }} className = "nav-header">
               Welcome
             </li>
-            <li onClick={() => { this.handleScrollTo(this.refEthics) }}>
+            <li onClick={() => { this.handleScrollTo(this.refEthics) }} className = "nav-header">
               Reporting and Journalism Ethics 
             </li>
-            <li onClick={() => { this.handleScrollTo(this.refWashU) }}>
+            <li onClick={() => { this.handleScrollTo(this.refWashU) }} className = "nav-header">
               Washington University 101 
             </li>            
-            <li onClick={() => { this.handleScrollTo(this.refWritingReporting) }}>
+            <li onClick={() => { this.handleScrollTo(this.refWritingReporting) }} className = "nav-header">
               Writing from Reporting 
             </li>
-            <li onClick={() => { this.handleScrollTo(this.refWritingExperience) }}>
+            <li onClick={() => { this.handleScrollTo(this.refWritingExperience) }} className = "nav-header">
             Writing from Experience 
             </li>
           </ul>
         </nav>
         <div className="spacer"></div>
         <div ref={this.refWelcome}>
-          <h2>Welcome to Student Life</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <h2 className = "nav-content-header">Welcome to Student Life</h2>
+          <p className = "nav-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
         <div className="spacer"></div>
         <div ref={this.refEthics}>
-          <h2>Journalism Ethics</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <h2 className = "nav-content-header">Journalism Ethics</h2>
+          <p className = "nav-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
         <div className="spacer"></div>
         <div ref={this.refWashU}>
-          <h2>Washigton University 101</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <h2 className = "nav-content-header">Washigton University 101</h2>
+          <p className = "nav-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
         <div className="spacer"></div>
         <div ref={this.refWritingReporting}>
-          <h2>Writing from Reporting</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <h2 className = "nav-content-header">Writing from Reporting</h2>
+          <p className = "nav-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
         <div ref={this.refWritingExperience}>
-          <h2>Writing from Experience</h2>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <h2 className = "nav-content-header">Writing from Experience</h2>
+          <p className = "nav-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
       </article>     
       );
