8be7f5557afd7c042c577da4aad44ec9bcb32b3b
Emma Baker <emma.baker@wustl.edu>
2020-09-01 23:55:23 -0500

    This was such a silly problem

--- studlife-training/src/components/Nav.js
+++ studlife-training/src/components/Nav.js
@@ -2,22 +2,71 @@ import React, { Component } from 'react'
 import {Route, Switch} from "react-router";
 import Scroll from 'react-scroll'
 import { HashLink as Link } from 'react-router-hash-link';
-import './Nav.css';
-import ReadMoreReact from 'read-more-react';
-// import { reportingText } from './text/reportingText.txt'; 
 
+import ReadMoreReact from 'read-more-react';
+
+import ReactMarkdown from 'react-markdown';
+import reportingMarkdown from './assets/reportingText.md';
+import reportingActivityMarkdown from './assets/reportingActivity.md';
+import newsMarkdown from './assets/newsText.md';
+import newsActivityMarkdown from './assets/newsActivity.md';
+import experienceMarkdown from './assets/experienceText.md';
+import experienceActivityMarkdown from './assets/experienceActivity.md';
+import washUMarkdown from './assets/washUText.md';
+import washUActivityMarkdown from './assets/washUActivity.md';
+
+import './Nav.css';
 
 
 class Nav extends Component {
+  constructor(props){
+    super(props)
+    this.state = { reportingText: null }
+    this.state = { reportingActivityMarkdown: null }
+    this.state = { newsMarkdown: null }
+    this.state = { newsActivityMarkdown: null }
+    this.state = { experienceMarkdown: null }
+    this.state = { experienceActivityMarkdown: null }
+    this.state = { washUMarkdown: null }
+    this.state = { washUActivityMarkdown: null }
+  }
   refWelcome = React.createRef()
   refEthics = React.createRef()
   refWashU = React.createRef()
   refWritingReporting = React.createRef()
   refWritingExperience = React.createRef()
   refSubmit= React.createRef()
+  
+
   componentDidMount() {
     this.setState(
+      
       { loaded: true })
+    
+    fetch(reportingMarkdown).then((response) => response.text()).then((text) => {
+      this.setState({ reportingText: text })
+    })
+    fetch(reportingActivityMarkdown).then((response) => response.text()).then((text) => {
+      this.setState({ reportingActivityMarkdown: text })
+    })
+    fetch(newsMarkdown).then((response) => response.text()).then((text) => {
+      this.setState({ newsMarkdown: text })
+    })
+    fetch(newsActivityMarkdown).then((response) => response.text()).then((text) => {
+      this.setState({ newsActivityMarkdown: text })
+    })
+    fetch(experienceMarkdown).then((response) => response.text()).then((text) => {
+      this.setState({ experienceMarkdown: text })
+    })
+    fetch(experienceActivityMarkdown).then((response) => response.text()).then((text) => {
+      this.setState({ experienceActivityMarkdown: text })
+    })
+    fetch(washUMarkdown).then((response) => response.text()).then((text) => {
+      this.setState({ washUMarkdown: text })
+    })
+    fetch(washUActivityMarkdown).then((response) => response.text()).then((text) => {
+      this.setState({ washUActivityMarkdown: text })
+    })
   }
   handleScrollTo = (elRef) => {
     const el = elRef.current ? elRef.current : elRef
@@ -26,14 +75,6 @@ class Nav extends Component {
       block: 'start'
     })
   }
-
-  handleReportingClick = () => {
-    fetch('./text/reportingText.txt')
-    .then((r) => r.text())
-    .then(text  => {
-      console.log(text);
-    })  
-  } 
 
   executeOnClick(isExpanded) {
     console.log(isExpanded);
@@ -70,31 +111,33 @@ class Nav extends Component {
         <div className="spacer"></div>
         <div ref={this.refWelcome}>
           <h2 className = "main-header">Welcome to Student Life</h2>
-          <p className = "nav-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-        
         </div>
         <div className="spacer"></div>
         <div ref={this.refEthics}>
           <h2 className = "nav-content-header">Journalism Ethics</h2>
-          <p className = "nav-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <ReactMarkdown className = "nav-content" source={this.state.reportingText} />
+          <ReactMarkdown className = "nav-content" source={this.state.reportingActivityMarkdown} />        
           </div>
         <div className="spacer"></div>
         <div ref={this.refWashU}>
           <h2 className = "nav-content-header">Washigton University 101</h2>
-          <p className = "nav-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <ReactMarkdown className = "nav-content" source={this.state.washUMarkdown} />
+          <ReactMarkdown className = "nav-content" source={this.state.washUActivityMarkdown} />
         </div>
         <div className="spacer"></div>
         <div ref={this.refWritingReporting}>
           <h2 className = "nav-content-header">Writing from Reporting</h2>
-          <p className = "nav-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <ReactMarkdown className = "nav-content" source={this.state.newsMarkdown} />
+          <ReactMarkdown className = "nav-content" source={this.state.newsActivityMarkdown} />
+
         </div>
         <div ref={this.refWritingExperience}>
           <h2 className = "nav-content-header">Writing from Experience</h2>
-          <p className = "nav-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <ReactMarkdown className = "nav-content" source={this.state.experienceMarkdown} />
+          <ReactMarkdown className = "nav-content" source={this.state.experienceActivityMarkdown} />
         </div>
         <div ref={this.refSubmit}>
           <h2 className = "nav-content-header">Submit</h2>
-          <p className = "nav-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
         </div> 
       </article>  
